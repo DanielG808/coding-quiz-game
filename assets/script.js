d26f9bc8e40887a9c862836532490d7c08318bac
Dan G <daniel.w.gray1994@gmail.com>
2023-05-22 16:49:51 -0400

    add a var called questions; add a function called displayQuestion()

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,26 @@
 var startButton = document.querySelector(".start");
 var timeLeftEl = document.querySelector(".timer");
 var h1El = document.querySelector(".heading");
+var pEl = document.querySelector(".sub-heading")
 
 // DATA / STATE
 
+var userScore = 10;
+var questions = [
+    {
+        question: "What is JavaScript?"
+        option1: "A font-style developed in coffee shops during the 1600s"
+        option2: "A legal document governing modern coffee supply-chain laws"
+        option3: "An object-oriented programming language first released in 1995"
+        option4: "A technique barista's use to make latte art"
+        key: 4
+    }
+]
+
 // FUNCTIONS
+
+function getUserScore() {
+}
 
 function gameOver() {
     // end the game
@@ -14,7 +30,10 @@ function gameOver() {
     // display message for out of time
     h1El.textContent = "You're out of time!";
     // store the score locally
-    // display score for this round 
+    currentScore = localStorage.setItem("score", userScore);
+    // display score for this round (could add if statement that tells user they did good, ok, bad based on score)
+    pEl.textContent = "You got " + userScore + " out of " + questions.length + " correct!"
+
 }
 
 function startTimer() {
@@ -34,11 +53,16 @@ function startTimer() {
     
 };
 
+function displayQuestion(
+
+)
+
 function startGame() {
     console.log("The game has started.");
     // start the timer
     startTimer();
     // display first question and options
+    displayQuestion();
 };
 
 // USER INTERACTIONS
